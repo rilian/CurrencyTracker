6077afbfad601bc0bbf8024943b365f952e92494
rilian <dmitriyis@gmail.com>
2012-10-15 01:09:53 +0300

    updating rails configs to latest version

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CurrencyTracker::Application.config.session_store :cookie_store, :key => '_currency_tracker_session'
+CurrencyTracker::Application.config.session_store :cookie_store, key: '_currency_tracker_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
